2b1b199696fb75e40b8d5fc9cc56d6ebe0ceb947
gilmutdinov.t3 <gilmutdinov.t3@rwb.ru>
2026-02-23 13:31:04 +0300

    sql_queries
    sql_queries
    
    sql_queries

--- sql_queries.py
+++ sql_queries.py
@@ -2,6 +2,10 @@
 # Duration - разница между scheduled_arrival и scheduled_departure.
 # В ответе должно быть 2 колонки [flight_no, duration]
 TASK_1_QUERY = """
+SELECT flight_no, (scheduled_arrival - scheduled_departure) AS duration
+FROM flights
+ORDER BY duration ASC
+LIMIT 5;
 """
 #  flight_no | duration
 # -----------+----------
@@ -16,6 +20,12 @@ TASK_1_QUERY = """
 # количество упоминаний которых меньше 50
 # В ответе должно быть 2 колонки [flight_no, count]
 TASK_2_QUERY = """
+select flight_no, COUNT(flight_no)
+from flights
+group by flight_no
+having COUNT(flight_no) < 50
+order by COUNT(flight_no) desc
+limit 3;
 """
 #  flight_no | count
 # -----------+-------
@@ -26,6 +36,11 @@ TASK_2_QUERY = """
 # Вывести число перелетов внутри одной таймзоны
 # Нужно вывести 1 значение в колонке count
 TASK_3_QUERY = """
+select COUNT(f.flight_no) as count
+from flights f
+inner join airports_data dep on dep.airport_code = f.departure_airport
+inner join airports_data arr on arr.airport_code = f.arrival_airport
+where dep.timezone = arr.timezone;
 """
 #  count
 # --------
